7d6c65e94b7a9e1f2d22211bdd47a703262eb9ce
P4R90 <40534587@live.napier.ac.uk>
2023-03-28 08:08:05 +0100

    k

---
+++ scripts/recipe_script.js
@@ -0,0 +1,57 @@
+function openPopup() {
+  // Specify the URL of the pop-up window
+  const url = 'mexicana.html';
+
+  // Specify the size and position of the pop-up window
+  const width = 700;
+  const height = 430;
+  const left = (window.innerWidth - width) / 2;
+  const top = (window.innerHeight - height) / 2;
+
+  // Open the pop-up window
+    window.open(url, '_blank', `width=${width}, height=${height}, left=${left}, top=${top}, resizable=no`);
+}
+
+
+function openPopup2() {
+  // Specify the URL of the pop-up window
+  const url = 'americana.html';
+
+  // Specify the size and position of the pop-up window
+  const width = 700;
+  const height = 430;
+  const left = (window.innerWidth - width) / 2;
+  const top = (window.innerHeight - height) / 2;
+
+  // Open the pop-up window
+    window.open(url, '_blank', `width=${width}, height=${height}, left=${left}, top=${top}, resizable=no`);
+}
+
+function openPopup3() {
+  // Specify the URL of the pop-up window
+  const url = 'hawaiian.html';
+
+  // Specify the size and position of the pop-up window
+  const width = 700;
+  const height = 430;
+  const left = (window.innerWidth - width) / 2;
+  const top = (window.innerHeight - height) / 2;
+
+  // Open the pop-up window
+    window.open(url, '_blank', `width=${width}, height=${height}, left=${left}, top=${top}, resizable=no`);
+}
+
+
+function openPopup4() {
+  // Specify the URL of the pop-up window
+  const url = 'hot_burger.html';
+
+  // Specify the size and position of the pop-up window
+  const width = 700;
+  const height = 430;
+  const left = (window.innerWidth - width) / 2;
+  const top = (window.innerHeight - height) / 2;
+
+  // Open the pop-up window
+    window.open(url, '_blank', `width=${width}, height=${height}, left=${left}, top=${top}, resizable=no`);
+}
